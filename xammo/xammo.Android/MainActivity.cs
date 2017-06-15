af85763201d0377e5e78c3a0e8acfae9b1343ea2
dknr <me@dknauer.me>
2017-06-15 09:24:11 -0700

    initial commit

---
+++ xammo/xammo.Android/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+namespace Xammo.Droid
+{
+	[Activity (Label = "xammo", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : FormsAppCompatActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			Forms.Init (this, bundle);
+			LoadApplication (new App ());
+		}
+	}
+}
+
